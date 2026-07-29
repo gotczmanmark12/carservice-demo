d2438c9e7c93a5b6a4cfe2b1f375d2806ba46fed
gotczmanmark12 <gotczmanmark69@gmail.com>
2026-07-29 11:30:18 +0300

    build navbar & hamburger menu

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
